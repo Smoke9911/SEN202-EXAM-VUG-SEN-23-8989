e357a2b4d8caba9e98229106d9c9c958f8b982bb
VUG/SEN/23/8989 <VUG/SEN/23/8989@veritas.edu.ng>
2025-06-28 11:23:16 +0100

    Question 3

--- employees/models.py
+++ employees/models.py
@@ -22,3 +22,13 @@ class Intern(StaffBase):
     def get_role(self):
         return "Intern"
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True)
+    intern = models.ForeignKey(Intern, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}"
--- employees/serializers.py
+++ employees/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Manager, Intern
+from .models import Address
 
 class ManagerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,8 @@ class InternSerializer(serializers.ModelSerializer):
     class Meta:
         model = Intern
         fields = ['id', 'full_name', 'email', 'mentor', 'internship_end', 'get_role']  # I excluded sensitive fields here
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
